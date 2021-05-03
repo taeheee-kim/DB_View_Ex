7dfe84019a0635eea234f712cd829ad81c350f2d
taeheeeeeeeeeeeeeeeee <68436856+taeheeeeeeeeeeeeeeeee@users.noreply.github.com>
2021-05-03 12:33:55 +0900

    Add codes on ChatActivity.java - Write a message to the database

--- app/src/main/java/com/example/db_view_ex/ChatActivity.java
+++ app/src/main/java/com/example/db_view_ex/ChatActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance(); //선언과 생성
+        DatabaseReference myRef = database.getReference("message"); //message라는 곳에 접근하기
+
+        myRef.setValue("Hello, World!"); //값을 집어넣기
     }
 }
