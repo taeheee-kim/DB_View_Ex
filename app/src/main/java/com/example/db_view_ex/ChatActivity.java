a93f9a2fed4097ae362d513b7a262168d831a5ae
taeheeeeeeeeeeeeeeeee <68436856+taeheeeeeeeeeeeeeeeee@users.noreply.github.com>
2021-05-11 15:45:04 +0900

    1. 화면에 EditText & Button 추가 2. Button 누르면 Realtime Database로 멘트가 update 되도록 설정

--- app/src/main/java/com/example/db_view_ex/ChatActivity.java
+++ app/src/main/java/com/example/db_view_ex/ChatActivity.java
@@ -3,28 +3,38 @@ package com.example.db_view_ex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
 
+    EditText add_comment;
+    String comment;
+    Button btn_add_comment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance(); //선언과 생성
-        DatabaseReference myRef = database.getReference("message"); //message라는 곳에 접근하기
+        add_comment = findViewById(R.id.add_comment);
+        btn_add_comment = findViewById(R.id.btn_add_comment);
 
-        myRef.setValue("Hello, JIWON"); //값을 집어넣기
+        btn_add_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comment = add_comment.getText().toString();
 
-        //1. RECYCLERVIEW - 반복
-        //2. DB 내용을 넣는다
-        //3. 상대방폰에 채팅 내용이 보임
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("택배");
 
-        //1-1. RECYCLERVIEW - CHAT DATA
-        // 1. MESSAGE, NICKNAME, ISMINE
+                myRef.setValue(comment);
+            }
+        });
+
     }
 }
